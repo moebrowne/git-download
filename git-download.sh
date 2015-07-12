606477916383cbced01e7a79e3cd072eafff38f9
MoeBrowne <oliver.browne@uplyme.com>
2015-07-12 19:33:01 +0100

    Improved the are you sure message when the target directory exists

--- git-download.sh
+++ git-download.sh
@@ -41,7 +41,7 @@ DOWN_DIR="$DOWN_DIR/$REPO_NAME"
 # Check if that directory already exists
 if [[ -d "$DOWN_DIR" ]]; then
 	# Check its ok to overwrite this directory
-	read -r -p "Are you sure? [y/N] " DOWN_DIR_DELETE
+	read -r -p "$DOWN_DIR already exists, overwrite? [y/N] " DOWN_DIR_DELETE
 	DOWN_DIR_DELETE=${DOWN_DIR_DELETE,,}    # tolower
 	if [[ $DOWN_DIR_DELETE =~ ^(yes|y)$ ]]; then
 		echo "Deleting $DOWN_DIR"
