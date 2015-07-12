ad7d0e3d0b1923f0853d25f879ca4e1da9c89daa
MoeBrowne <oliver.browne@uplyme.com>
2015-07-12 18:41:35 +0100

    Added regex parameter matching for --branch/-b --repo/-r

--- git-download.sh
+++ git-download.sh
@@ -1,1 +1,17 @@
 #!/bin/bash
+
+regexArgType=' -(-repo|r) ([^ ]+) '
+[[ $args =~ $regexArgType ]]
+if [ "${BASH_REMATCH[2]}" != "" ]; then
+	REPO_URL="${BASH_REMATCH[2]}"
+else
+	REPO_URL="rsa"
+fi
+
+regexArgType=' -(-branch|b) ([^ ]+) '
+[[ $args =~ $regexArgType ]]
+if [ "${BASH_REMATCH[2]}" != "" ]; then
+	REPO_BRANCH="${BASH_REMATCH[2]}"
+else
+	REPO_BRANCH="master"
+fi
